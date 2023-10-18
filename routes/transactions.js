bd6851715b3a2f2cb8d9006c65658d79601ae267
Sanjaykumar <sanjaykumar24official@gmail.com>
2023-10-18 15:34:27 +0530

    updation added

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router();
-const {AdminverifyMiddleware} =require('./verifyMiddleware')
-const {UserverifyMiddleware} = require('./verifyMiddleware');
+const {AdminverifyMiddleware}=require('./verifyMiddleware')
+const {UserverifyMiddleware}=require('./verifyMiddleware');
 const userModel = require('../schema/user');
 router.post('/recharge', AdminverifyMiddleware, async (req, res) => {
   const { rollno, rechargeamount } = req.body;
