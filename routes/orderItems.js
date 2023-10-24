e075a82b59166fcbdfdcc1028bd21d969155da29
Sri Harish <sriharish7635@gmail.com>
2023-10-24 12:07:31 +0530

    addition of Transaction  to user order

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const userModel = require("../schema/user");
 const categoryModel = require("../schema/products");
 const UserOrderModel = require("../schema/userOrder");
@@ -11,19 +12,25 @@ const adminModel = require("../schema/admin");
 const router = express.Router();
 
 router.post("/user", UserverifyMiddleware, async (req, res) => {
-  try {
-  const userId = req.userId;
-  const { orders, totalPrice } = req.body;
+  const session = await mongoose.startSession();
 
-  if (!orders || !totalPrice || totalPrice <= 0) {
-    return res.status(422).json({ message: "Invalid details" });
-  }
-  
+  try {
+    //! Transaction Starts Since it edits the the DB
+    session.startTransaction();
+    const userId = req.userId;
+    const { orders, totalPrice } = req.body;
+
+    if (!orders || !totalPrice || totalPrice <= 0) {
+      session.endSession();
+      return res.status(422).json({ message: "Invalid details" });
+    }
+
     let amount = 0;
 
     const userBal = await userModel.findById(userId, "amount");
 
     if (userBal.amount < totalPrice) {
+      session.endSession();
       return res.status(422).json({ message: "Insufficirnt balance" });
     }
     const userOrders = [];
@@ -43,12 +50,14 @@ router.post("/user", UserverifyMiddleware, async (req, res) => {
       );
 
       if (result[0].categorydetails[0].productstock < order.quantity) {
+        session.endSession();
         return res.status(503).json({
           message: `${result[0].categorydetails[0].productname} not available to mentioned your quantity`,
         });
       }
       amount += result[0].categorydetails[0].productprice * order.quantity;
       if (userBal < amount) {
+        session.endSession();
         return res
           .status(422)
           .json({ message: "Insufficient balance while billing" });
@@ -73,6 +82,7 @@ router.post("/user", UserverifyMiddleware, async (req, res) => {
     }
 
     if (amount != totalPrice) {
+      session.endSession();
       return res.status(500).send({ Message: "Calculation Err!" });
     }
 
@@ -100,28 +110,37 @@ router.post("/user", UserverifyMiddleware, async (req, res) => {
     const add = new UserOrderModel({
       userId: userId,
       orders: orderHistory,
-      totalPrice:amount,
+      totalPrice: amount,
       date: Date.now(),
     });
 
     const status = await add.save();
     console.log(status);
     res.json({ userOrders, totalamount: amount });
+    await session.commitTransaction();
+    session.endSession();
   } catch (err) {
-    console.log("error billing :"+error.message);
-    return res.status(500).send({ message: "internal server error =====>" + error.message});
+    console.log("error billing :" + error.message);
+
+    await session.abortTransaction();
+    session.endSession();
+    return res
+      .status(500)
+      .send({ message: "internal server error =====>" + error.message });
   }
 });
 
 router.post("/admin", AdminverifyMiddleware, async (req, res) => {
+  const session = await mongoose.startSession();
   try {
-  const userId = req.userId;
-  const { orders, totalPrice, rollno } = req.body;
+    session.startTransaction();
+    const userId = req.userId;
+    const { orders, totalPrice, rollno } = req.body;
 
-  if (!orders || !totalPrice || totalPrice <= 0 || !rollno) {
-    return res.status(422).json({ message: "Invalid details" });
-  }
-  
+    if (!orders || !totalPrice || totalPrice <= 0 || !rollno) {
+      return res.status(422).json({ message: "Invalid details" });
+    }
+
     let amount = 0;
 
     const userBal = await userModel.findOne({ rollno }, "amount");
@@ -206,16 +225,22 @@ router.post("/admin", AdminverifyMiddleware, async (req, res) => {
       admin: adminMail.email,
       userId: rollno,
       orders: orderHistory,
-      totalPrice:amount,
+      totalPrice: amount,
       date: Date.now(),
     });
 
     const status = await add.save();
     console.log(status);
     res.json({ userOrders, totalamount: amount });
+    await session.commitTransaction();
+    session.endSession();
   } catch (err) {
-    console.log("error :"+err.message);
-    return res.status(500).send({ message: "internal server error =====>" + err.message});
+    console.log("error :" + err.message);
+    await session.abortTransaction();
+    session.endSession();
+    return res
+      .status(500)
+      .send({ message: "internal server error =====>" + err.message });
   }
 });
 
