96b5a1785701e1004b598d80a5c402352fcf171a
Sheshadri <vijaysai@example.com>
2023-10-27 18:48:57 +0530

    shesha orders

--- schema/orders.js
+++ schema/orders.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
-function date() {
-  const now = new Date();
-  const options = {
-    timeZone: "Asia/Kolkata",
-    hour12: false,
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  };
-  const istTime = now.toLocaleString("en-IN", options);
-  return istTime;
+function date()
+{
+    const now = new Date();
+    const options = {
+        timeZone: 'Asia/Kolkata',
+        hour12: false,
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    const istTime = now.toLocaleString('en-IN', options);
+    return istTime;
 }
 const orderSchema = new mongoose.Schema({
   category_id: {
