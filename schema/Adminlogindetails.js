409a099ddd2f2241fb56307e0e37334291f902b6
Sheshadri <vijaysai@example.com>
2023-10-29 21:16:53 +0530

    shesha date function updated

--- schema/Adminlogindetails.js
+++ schema/Adminlogindetails.js
@@ -1,20 +1,5 @@
 const mongoose = require('mongoose');
-function date()
-{
-    const now = new Date();
-    const options = {
-        timeZone: 'Asia/Kolkata',
-        hour12: false,
-        year: 'numeric',
-        month: 'short',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    };
-    const istTime = now.toLocaleString('en-IN', options);
-    return istTime;
-}
+const date = require("../routes/date");
 const loginSchema = new mongoose.Schema({
     email: {
         type: String,
