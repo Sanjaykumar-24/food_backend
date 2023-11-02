13a9b939dc53518c2fd002cbf0d0fd36cc65776a
Sri Harish <sriharish7635@gmail.com>
2023-11-02 14:06:41 +0530

    unique changed

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -328,7 +328,7 @@ router.post("/register", async(req, res) => {
 
         const data = await adminModel.create({ email, password: hashedPassword });
         console.log(data);
-        console.log(new Date(data.date))
+        console.log(String(data.date))
         const userid = { id: data.id };
         const accessToken = generrateAccessToken(userid);
         const refreshToken = generateRefreshToken(userid);
